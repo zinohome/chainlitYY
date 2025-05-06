c4b97b070cdf668f1515d4a982a41ef18bc8c1fb
zhangjun <ibmzhangjun@139.com>
2025-05-06 17:15:50 +0800

    updated

--- docker/bd_build/50_start_h.sh
+++ docker/bd_build/50_start_h.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-FIND_FILE="/opt/crtool/apprun/hypercorn.py"
-FIND_STR="workers"
-if [ `grep -c "$FIND_STR" $FIND_FILE` -ne '0' ];then
-    echo "hypercorn config exist"
-else
-    cp /opt/crtool/apprun/hypercorn_default.py /opt/crtool/apprun/hypercorn.py
-fi
-
-cd /opt/crtool/backend && \
-nohup /opt/crtool/venv/bin/hypercorn -c /opt/crtool/apprun/hypercorn.py main:app >> /tmp/crtool.log 2>&1 &
+cd /opt/chainlitYY && \
+. venv/bin/activate && \
+nohup /opt/chainlitYY/venv/bin/chainlit run app.py -h --host 0.0.0.0 --port 8000 >> /tmp/chainlitYY.log 2>&1 &
--- docker/bd_build/install.sh
+++ docker/bd_build/install.sh
@@ -11,16 +11,8 @@ cd /opt && \
 git clone https://github.com/zinohome/chainlitYY.git && \
 cd /opt/chainlitYY && \
 git pull && \
-chmod 755 mkcert-v1.4.4-linux-amd64 && mv mkcert-v1.4.4-linux-amd64 mkcert && mv mkcert /usr/bin/ && \
-mkcert -install && \
-mkdir -p /opt/crtool/backend/log && \
-mkdir -p /opt/crtool/cert && \
-mkcert -cert-file /opt/crtool/cert/cert.pem -key-file /opt/crtool/cert/key.pem ibmtls.com crtool.ibmtls.com localhost 127.0.0.1 ::1 && \
-cd /opt/crtool && \
 virtualenv venv && \
 . venv/bin/activate && \
 pip install -r requirements.txt && \
-cp /opt/crtool/docker/bd_build/wait-for /usr/bin/wait-for && chmod 755 /usr/bin/wait-for && \
-ls -l /opt/crtool/docker/bd_build/ && \
-cp /opt/crtool/docker/bd_build/50_start_h.sh /etc/my_init.d/50_start_crtool.sh &&
-chmod 755 /etc/my_init.d/50_start_crtool.sh
+cp /opt/crtool/docker/bd_build/50_start_h.sh /etc/my_init.d/50_start_h.sh &&
+chmod 755 /etc/my_init.d/50_start_h.sh
